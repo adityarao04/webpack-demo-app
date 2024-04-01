81ab75314b53d98abd55a59342775ae08c1f1f25
Aditya Rao <adityarao@ip-192-168-1-5.ap-south-1.compute.internal>
2024-04-01 23:34:09 +0530

    multiple entry points

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");  // already present in we
 module.exports = merge(common,{
     mode: "production",
     output: {
-        filename:"main.[contentHash].js",
+        filename:"[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, './dist')
     },
     optimization:{
