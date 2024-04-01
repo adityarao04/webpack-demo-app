5598289b119901938337d9914b2d078ce4caf3fc
Aditya Rao <adityarao@ip-192-168-1-5.ap-south-1.compute.internal>
2024-04-01 22:48:25 +0530

    minify css files

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,19 @@ const common = require('./webpack.common')
 const merge = require('webpack-merge')
 const CleanWebpackPlugin  = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");  // already present in webpack // need to add this as OptimizeCssAssetsPlugin overrides and JS is not minified
 module.exports = merge(common,{
     mode: "production",
     output: {
         filename:"main.[contentHash].js",
         path: path.resolve(__dirname, './dist')
+    },
+    optimization:{
+        minimizer: [ 
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ]
     },
     plugins:[
         new CleanWebpackPlugin(),
