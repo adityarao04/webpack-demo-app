17e9cd7100ebcc7e271845c553f08ea622498726
Aditya Rao <adityarao@ip-10-5-49-95.ap-south-1.compute.internal>
2024-04-01 17:11:38 +0530

    CleanWebpackPlugin and miniCssExtract - get css files

--- webpack.common.js
+++ webpack.common.js
@@ -15,13 +15,6 @@ module.exports = {
             // use: ['style-loader','css-loader']   //order of execution -> reverse order [......,2nd , 1st]
             // }
             {
-                test: /\.scss$/,
-                use: [
-                  "style-loader", //3. Inject styles into DOM
-                  "css-loader", //2. Turns css into commonjs
-                  "sass-loader" //1. Turns sass into css
-                ]
-              },{
                 test: /\.html$/,
                 use: ["html-loader"]
                 
