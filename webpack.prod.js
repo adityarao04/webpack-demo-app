e2b3e8ffde06cf08b89a973944d597b2033c5332
Aditya Rao <adityarao@ip-10-5-49-95.ap-south-1.compute.internal>
2024-04-01 17:11:38 +0530

    CleanWebpackPlugin and miniCssExtract - get css files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common')
 const merge = require('webpack-merge')
-
+const CleanWebpackPlugin  = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 module.exports = merge(common,{
     mode: "production",
     output: {
         filename:"main.[contentHash].js",
         path: path.resolve(__dirname, './dist')
     },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" })
+    ]
+    ,  
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                  "css-loader", //2. Turns css into commonjs
+                  "sass-loader" //1. Turns sass into css
+                ]
+              }
+        ]
+    }
 })
